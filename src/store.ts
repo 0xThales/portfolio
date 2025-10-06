5167f4b6997d1ecf551b700f16fd65f7db8b27b8
0xThales <dan.console.log@gmail.com>
2025-10-06 23:06:07 +0200

    add mobile knob player

--- src/store.ts
+++ src/store.ts
@@ -9,3 +9,10 @@ export const sectionCount = atom<number>(0)
 export const gameOn = atom<boolean>(false)
 
 export const modalContentId = atom<string>("")
+
+export const knobState = atom<{ left: boolean; right: boolean; up: boolean; down: boolean }>({
+    left: false,
+    right: false,
+    up: false,
+    down: false
+})
